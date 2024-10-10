6f4a8cb25b0fecb160d51f1951e177f884d2c8dd
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-10-10 11:56:27 +0530

    adsflkj

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,14 +6,12 @@ local opt = vim.opt
 
 opt.conceallevel = 1
 opt.cmdheight = 0
-
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
+opt.relativenumber = false
 
 -- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = "ruff_lsp"
+vim.g.lazyvim_python_ruff = "ruff"
 
 vim.g.root_spec = { "cwd" }
 vim.g.omni_sql_no_default_maps = 1
